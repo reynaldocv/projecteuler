a0aa44a19716a7bbd1e26821908ba0248d34fc8a
reynaldocv <reynaldocv@yahoo.com>
2019-06-11 16:36:41 -0300

    Problem 1

--- PE-problem:001.py
+++ PE-problem:001.py
@@ -1,15 +1,14 @@
 #Problem 1: Multiples of 3 and 5
-
 import datetime
 
-def suma(m,n):
-	return m*((n-1)//m)*((n-1)//m+1)//2
-
 def program():
-	return suma(3,1000)+suma(5,1000)-suma(15,1000)
+	def suma(m, n):
+		return m*((n - 1)//m)*((n - 1)//m + 1)//2
+
+	return suma(3, 1000)+suma(5, 1000)-suma(15, 1000)
 
 if __name__ == "__main__":
 	h1 = datetime.datetime.now()	
-	rpta=program()
+	ans = program()
 	h2 = datetime.datetime.now()
-	print("Problem 001 (",h2-h1,") : ",rpta)
+	print("Problem 001 (",h2-h1,") : ",ans)
