d582d9c86c00062964d50b2d331d26ce62a85f86
reynaldocv <reynaldocv@yahoo.com>
2019-06-11 16:25:57 -0300

    1

---
+++ PE-problem:001.py
@@ -0,0 +1,15 @@
+#Problem 1: Multiples of 3 and 5
+
+import datetime
+
+def suma(m,n):
+	return m*((n-1)//m)*((n-1)//m+1)//2
+
+def program():
+	return suma(3,1000)+suma(5,1000)-suma(15,1000)
+
+if __name__ == "__main__":
+	h1 = datetime.datetime.now()	
+	rpta=program()
+	h2 = datetime.datetime.now()
+	print("Problem 001 (",h2-h1,") : ",rpta)
