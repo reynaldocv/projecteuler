5e90d61ee6f112056ce32057c98046a338597d98
reynaldocv <reynaldocv@yahoo.com>
2019-06-11 21:38:30 -0300

    problem 016

---
+++ PE-problem:016.py
@@ -0,0 +1,17 @@
+#Problem 016: Power digit sum
+import datetime
+
+def program():
+	nro = 2**1000
+	ans = 0
+	while nro > 0:
+		ans += nro%10	
+		nro //= 10
+	return ans
+
+if __name__ == "__main__":
+	h1 = datetime.datetime.now()
+	ans = program()
+	h2 = datetime.datetime.now()
+	print("Problem 015 (",h2 - h1,") : ", ans)
+
