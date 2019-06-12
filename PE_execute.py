8cf95d1af0826ac93263f83352bf68949c56a141
reynaldocv <reynaldocv@yahoo.com>
2019-06-12 02:18:29 -0300

    list of results

---
+++ PE_execute.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+def program():
+	for i in range(1,10):		
+		file = "PE-problem:00"+str(i)+".py"		
+		my_file = Path(file)
+		if my_file.is_file():
+			os.system("python3 "+file)
+
+	for i in range(11,100):		
+		file = "PE-problem:0"+str(i)+".py"		
+		my_file = Path(file)
+		if my_file.is_file():
+			os.system("python3 "+file)
+
+if __name__ == "__main__":
+	program()
+
