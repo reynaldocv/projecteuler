9e926dc433af2c917ce907a5f0be4fe1a419fb87
reynaldocv <reynaldocv@yahoo.com>
2019-06-11 21:49:46 -0300

    Problem 017

--- PE-problem:016.py
+++ PE-problem:016.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
 	h1 = datetime.datetime.now()
 	ans = program()
 	h2 = datetime.datetime.now()
-	print("Problem 015 (",h2 - h1,") : ", ans)
+	print("Problem 016 (",h2 - h1,") : ", ans)
 
---
+++ PE-problem:017.py
@@ -0,0 +1,12 @@
+#Problem 018: Maximum path sum I
+import datetime
+
+def program():	
+	
+
+if __name__ == "__main__":
+	h1 = datetime.datetime.now()
+	ans = program()
+	h2 = datetime.datetime.now()
+	print("Problem 018 (",h2 - h1,") : ", ans)
+
