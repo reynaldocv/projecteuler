846d2f45083ff8e24ce53e6e432707fd217a934b
reynaldocv <37712690+reynaldocv@users.noreply.github.com>
2019-11-23 00:19:27 -0300

    Update PE-problem:001.py

--- PE-problem:001.py
+++ PE-problem:001.py
@@ -5,7 +5,7 @@ def program():
 	def suma(m, n):
 		return m*((n - 1)//m)*((n - 1)//m + 1)//2
 
-	return suma(3, 1000)+suma(5, 1000)-suma(15, 1000)
+	return suma(3, 1000) + suma(5, 1000) - suma(15, 1000)
 
 if __name__ == "__main__":
 	h1 = datetime.datetime.now()	
