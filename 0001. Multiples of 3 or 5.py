3036c2efa0e149b6353d2b83aedcb67374f1487a
reynaldocv <reynaldocv@gmail.com>
2021-12-03 00:15:45 -0500

    0001

--- 0001. Multiples of 3 or 5.py
+++ 0001. Multiples of 3 or 5.py
@@ -2,9 +2,9 @@ class solution:
 	def solution0001(self, n): 
 		ans = 0
 
-    for num in range(3, n):
-			if num % 3 == 0 or num % 5 == 0: 
-				ans += num
+		for num in range(3, n):
+				if num % 3 == 0 or num % 5 == 0: 
+					ans += num
 
 		return ans
 
